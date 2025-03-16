31da1e333f117cf1cb0b637bd44d5474fbc5af0b
cherif <cherif2862@gmail.com>
2025-03-16 11:19:24 +0000

    commit exo8

---
+++ Day#1/exo8.py
@@ -0,0 +1,7 @@
+print("Donner deux nombre ")
+nbr1 = float(input("le premier nombre "))
+nbr2 = float(input("le deuxieme nombre "))
+somme = nbr1 + nbr2
+print(f"La sommes des nombres est : {somme}")
+
+
