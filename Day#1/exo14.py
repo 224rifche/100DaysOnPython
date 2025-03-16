2496065061b1637b52217656e8ab3537c4b30650
cherif <cherif2862@gmail.com>
2025-03-16 14:07:48 +0000

    coomit exo14

---
+++ Day#1/exo14.py
@@ -0,0 +1,5 @@
+print("Saisir deux nombre : ")
+nbr1 = int(input("Saisir le premier nombre : "))
+nbr2 = int(input("Saisir le deuxieme nombre : "))
+multi = nbr1 * nbr2
+print(f"la multiplication est {multi}")
